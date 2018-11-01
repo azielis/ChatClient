44ac2c9acf9619ec2f97feec8cdb7a131f7c862e
Iryna <azielis93@gmail.com>
2018-11-01 14:40:28 +0100

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,13 @@
 package com.company;
 
 import javax.swing.*;
+import javax.swing.text.DefaultCaret;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -12,9 +15,12 @@ public class Main {
 
     Socket socket;
     JTextField message;
+    JTextArea odebraneWiadomosci;
     PrintWriter writer;
     JFrame mainFrame;
     JButton buttonWyslij;
+    BufferedReader czytelnik;
+    String nickName;
 
     public static void main(String[] args) {
         new Main().doDziela();
@@ -23,8 +29,20 @@ public class Main {
     private void doDziela() {
 
         mainFrame = new JFrame("Smieszny klient");
+        JPanel panelGlowny = new JPanel();
+        odebraneWiadomosci = new JTextArea(15, 50);
+        odebraneWiadomosci.setLineWrap(true);
+        odebraneWiadomosci.setWrapStyleWord(true);
+        odebraneWiadomosci.setEditable(false);
+
+        JScrollPane przewijanie = new JScrollPane(odebraneWiadomosci);
+        DefaultCaret caret = (DefaultCaret) odebraneWiadomosci.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        przewijanie.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        przewijanie.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setSize(400, 90);
+        mainFrame.setSize(530, 350);
 
         Box buttonBox = new Box(BoxLayout.LINE_AXIS);
         Box messageBox = new Box(BoxLayout.LINE_AXIS);
@@ -36,8 +54,14 @@ public class Main {
         buttonBox.add(buttonWyslij);
         messageBox.add(message);
 
-        mainFrame.getContentPane().add(BorderLayout.CENTER, messageBox);
-        mainFrame.getContentPane().add(BorderLayout.EAST, buttonBox);
+        mainFrame.getContentPane().add(BorderLayout.CENTER, panelGlowny);
+        //mainFrame.getContentPane().add(BorderLayout.EAST, buttonBox);
+
+        do {
+            nickName = JOptionPane.showInputDialog("Enter your nickname: ");
+        } while (nickName == null);
+
+        JLabel nickLabel = new JLabel(nickName);
 
         mainFrame.setVisible(true);
 
@@ -54,19 +78,45 @@ public class Main {
             }
         });
         buttonWyslij.addActionListener(a->{
-            writer.println(message.getText());
+            writer.println(nickLabel.getText() + ": " + message.getText());
             writer.flush();
             message.setText("");
             message.requestFocus();
         });
 
+        panelGlowny.add(nickLabel);
+        panelGlowny.add(przewijanie);
+        panelGlowny.add(message);
+        panelGlowny.add(buttonWyslij);
+
+
         konfigurujPolaczenieSieciowe();
+
+        Thread thread = new Thread(() -> {
+            String wiadom;
+
+            try {
+                while ((wiadom = czytelnik.readLine()) != null) {
+                    System.out.println("Odczytano " + wiadom);
+                    odebraneWiadomosci.append(wiadom + "\n");
+
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
+        thread.start();
+
+
+
     }
 
     private void konfigurujPolaczenieSieciowe() {
         try {
-            socket = new Socket("127.0.0.1", 4242);
+            socket = new Socket("127.0.0.1", 5000);
             writer = new PrintWriter(socket.getOutputStream());
+            InputStreamReader czytelnikStrm = new InputStreamReader(socket.getInputStream());
+            czytelnik = new BufferedReader(czytelnikStrm);
             System.out.println("Connected");
         } catch (IOException e) {
             e.printStackTrace();
